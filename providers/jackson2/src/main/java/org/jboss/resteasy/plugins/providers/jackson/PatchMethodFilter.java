ddcb94004d7817b8adf34b49f7d7200c1589cfce
Jim Ma <ema@redhat.com>
2017-04-26 14:17:59 +0800

    PatchMethodFilter is only executed when http method is patch and media type is application/json-patch+json

--- providers/jackson2/src/main/java/org/jboss/resteasy/plugins/providers/jackson/PatchMethodFilter.java
+++ providers/jackson2/src/main/java/org/jboss/resteasy/plugins/providers/jackson/PatchMethodFilter.java
@@ -9,6 +9,7 @@ import javax.ws.rs.ProcessingException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
 
 import org.jboss.resteasy.core.ResourceInvoker;
@@ -33,48 +34,49 @@ public class PatchMethodFilter implements ContainerRequestFilter
    @Override
    public void filter(ContainerRequestContext requestContext) throws IOException
    {
-      if (!requestContext.getMethod().equals("PATCH"))
+      //Strict the filter is only executed for patch method and media type is APPLICATION_JSON_PATCH_JSON_TYPE
+      if (requestContext.getMethod().equals("PATCH")
+            && requestContext.getMediaType().equals(MediaType.APPLICATION_JSON_PATCH_JSON_TYPE))
       {
-         return;
-      }
-      HttpRequest request = ResteasyProviderFactory.getContextData(HttpRequest.class);
-      HttpResponse response = ResteasyProviderFactory.getContextData(HttpResponse.class);
-      request.setHttpMethod("GET");
-      Registry methodRegistry = ResteasyProviderFactory.getContextData(Registry.class);
-      ResourceInvoker resourceInovker = methodRegistry.getResourceInvoker(request);
-      if (resourceInovker == null)
-      {
-         throw new ProcessingException("Get method not found and patch method failed");
-      }
-      ResourceMethodInvoker methodInvoker = (ResourceMethodInvoker) resourceInovker;
-      Object object = methodInvoker.invokeDryRun(request, response);
 
-      ByteArrayOutputStream tmpOutputStream = new ByteArrayOutputStream();
-      javax.ws.rs.ext.MessageBodyWriter msgBodyWriter = ResteasyProviderFactory.getInstance()
-            .getMessageBodyWriter(object.getClass(), object.getClass(), methodInvoker.getMethodAnnotations(),
-                  MediaType.APPLICATION_JSON_TYPE);
-      msgBodyWriter.writeTo(object, object.getClass(), object.getClass(), methodInvoker.getMethodAnnotations(),
-            MediaType.APPLICATION_JSON_TYPE, new MultivaluedTreeMap<String, Object>(), tmpOutputStream);
-      ObjectMapper mapper = new ObjectMapper();
-      JsonNode targetJson = mapper.readValue(tmpOutputStream.toByteArray(), JsonNode.class);
-      JsonPatch patch = JsonPatch.fromJson(mapper.readValue(request.getInputStream(), JsonNode.class));
+         HttpRequest request = ResteasyProviderFactory.getContextData(HttpRequest.class);
+         HttpResponse response = ResteasyProviderFactory.getContextData(HttpResponse.class);
+         request.setHttpMethod("GET");
+         Registry methodRegistry = ResteasyProviderFactory.getContextData(Registry.class);
+         ResourceInvoker resourceInovker = methodRegistry.getResourceInvoker(request);
+         if (resourceInovker == null)
+         {
+            throw new ProcessingException("Get method not found and patch method failed");
+         }
+         ResourceMethodInvoker methodInvoker = (ResourceMethodInvoker) resourceInovker;
+         Object object = methodInvoker.invokeDryRun(request, response);
 
-      JsonNode result = null;
-      try
-      {
-         result = patch.apply(targetJson);
+         ByteArrayOutputStream tmpOutputStream = new ByteArrayOutputStream();
+         MessageBodyWriter msgBodyWriter = ResteasyProviderFactory.getInstance().getMessageBodyWriter(
+               object.getClass(), object.getClass(), methodInvoker.getMethodAnnotations(),
+               MediaType.APPLICATION_JSON_TYPE);
+         msgBodyWriter.writeTo(object, object.getClass(), object.getClass(), methodInvoker.getMethodAnnotations(),
+               MediaType.APPLICATION_JSON_TYPE, new MultivaluedTreeMap<String, Object>(), tmpOutputStream);
+         ObjectMapper mapper = new ObjectMapper();
+         JsonNode targetJson = mapper.readValue(tmpOutputStream.toByteArray(), JsonNode.class);
+         JsonPatch patch = JsonPatch.fromJson(mapper.readValue(request.getInputStream(), JsonNode.class));
+
+         JsonNode result = null;
+         try
+         {
+            result = patch.apply(targetJson);
+         }
+         catch (JsonPatchException e)
+         {
+            throw new ProcessingException(e);
+         }
+         ByteArrayOutputStream targetOutputStream = new ByteArrayOutputStream();
+         mapper.writeValue(targetOutputStream, result);
+         request.setInputStream(new ByteArrayInputStream(targetOutputStream.toByteArray()));
+
+         request.setHttpMethod("PATCH");
+
       }
-      catch (JsonPatchException e)
-      {
-         throw new ProcessingException(e);
-      }
-      ByteArrayOutputStream targetOutputStream = new ByteArrayOutputStream();
-      mapper.writeValue(targetOutputStream, result);
-      request.setInputStream(new ByteArrayInputStream(targetOutputStream.toByteArray()));
-
-      request.setHttpMethod("PATCH");
-
-      return;
 
    }
 
